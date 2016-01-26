55e6ae4e328e19ef13cbce455c05a4d6bc891b10
nicola cabiddu (nik) <nicolacabiddu@gmail.com>
2016-01-26 21:55:06 +0000

    implemented

--- sample_code/tuple_to_function.cpp
+++ sample_code/tuple_to_function.cpp
@@ -1,71 +1,34 @@
+#include <cstddef>
 #include <tuple>
-#include <memory>
+#include <utility>
+#include <type_traits>
 #include <iostream>
-#include <string>
 
-template<typename F>
-class FunctorObj 
+template<typename F, typename T, size_t... S>
+inline
+auto apply(F&& fn, T&& t, std::index_sequence<S...>)
 {
-protected:
-	F _f;
-public:
-	FunctorObj(F f) : _f(std::move(f))
-	{}	
+	return std::forward<F>(fn)(std::get<S>(std::forward<T>(t))...);
+}
 
-   virtual ~FunctorObj() = default;
-   
-   virtual void toArgs() = 0;
-   
-   template<typename... Args>
-   auto getArgs(Args&&... args )
-   {
-      return std::make_tuple(std::forward<Args>(args)...);
-   }
-};
+template<typename F, typename T>
+inline
+auto run_f(F&&fn, T&& t)
+{
+	constexpr std::size_t tSize = 
+		std::tuple_size<typename std::remove_reference<T>::type>::value;
 
-template<typename F>
-class ImplFunctorObj : public FunctorObj<F>{
-   std::string _name;
-   int _num;
-public:
-
-   ImplFunctorObj(F f, std::string name, int num) : FunctorObj<F>(f),
-																	 _name(std::move(name)), 
-																	 _num(num)
-   {}
-   
-   virtual void toArgs()
-   {
-      auto t = getArgs(_name, _num);
-      
-      std::cout << std::get<0>(t) << " - " << std::get<1>(t) << std::endl;
-   }
-};
-
-
-///
-/// @brief: Takes a function and a list of params and executes f(args...)
-/// @param F : function to exec
-/// @param Args: arguments to pass to f
-///
-
-/*
-class Functor {
-   template<typename F, typename... Args  >
-   Functor( F f, Args&&... args ) {
-      auto argtuple = std::make_tuple(std::forward<Args>(args)...);
-      func(insert_magic_here(argtuple));
-   }
-};
-*/
-
-
+	return apply(std::forward<F>(fn), 
+			std::forward<T>(t),
+			std::make_index_sequence<tSize>());
+}
 
 int main(int argc, char** argv)
 {
-   std::unique_ptr<FunctorObj> ptr = std::make_unique<ImplFunctorObj>("hello", 10);
-   ptr->toArgs();
 
+	auto f = [](int a, int b){return a+b;};
+	auto sum = run_f(f, std::make_tuple(10,20));
+	std::cout << sum << std::endl;
    
    return 0;
 }
